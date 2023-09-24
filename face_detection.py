7c484736023bb7f54e2e4aa3326549a2cc9c33d1
Abigail Woolley <woolleyabigail47@gmail.com>
2023-09-24 19:45:17 +0000

    Added a face detector function/file

--- face_detection.py
+++ face_detection.py
@@ -1,43 +1,60 @@
 import os
+import cv2
+import dlib
 import shutil
-from deepface import DeepFace
+import argparse
 
 
-def process_images(input_dir, output_dir):
+# Function to check if a face exists in an image
+def has_face(image_path):
+    # Load the detector
+    detector = dlib.get_frontal_face_detector()
+    # Read the image
+    img = cv2.imread(image_path)
+
+    if img is None:
+        print(f"Error reading image: {image_path}")
+        return False
+
+    # Convert image into grayscale
+    gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+
+    # Use detector to find faces
+    faces = detector(gray)
+
+    # Return True if at least one face is detected, otherwise return False
+    return len(faces) > 0
+
+
+# Function to perform face detection and copying
+def perform_face_detection(input_directory, output_directory):
     # Create the output directory if it doesn't exist
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # Variables to keep track of counts
-    total_detected_faces = 0
-    total_no_face_detected = 0
-
-    # Function to check if a face exists in an image
-    def has_face(image_path):
-        try:
-            # Use DeepFace to detect faces in the image
-            result = DeepFace.analyze(image_path, actions=["emotion"])
-            return result["emotion"] is not None
-        except Exception as e:
-            print(f"Error processing {image_path}: {str(e)}")
-            return False
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     # Iterate through images in the input directory
-    for entry in os.scandir(input_dir):
-        if entry.is_file() and entry.name.lower().endswith((".jpg", ".jpeg", ".png")):
-            image_path = entry.path
-            output_path = os.path.join(output_dir, entry.name)
+    for filename in os.listdir(input_directory):
+        if filename.endswith((".jpg", ".jpeg", ".png")):
+            image_path = os.path.join(input_directory, filename)
+            output_path = os.path.join(output_directory, filename)
 
             # Check if a face exists in the image
             if has_face(image_path):
                 # If a face exists, copy the image to the output directory
                 shutil.copy(image_path, output_path)
-                total_detected_faces += 1
-                print(f"Face detected in {entry.name}.")
+                print(f"Face detected in {filename} and copied to the output directory.")
             else:
-                total_no_face_detected += 1
-                print(f"No face detected in {entry.name}.")
+                print(f"No face detected in {filename}.")
 
     print("Processing completed.")
-    print(f"Total detected faces: {total_detected_faces}")
-    print(f"Total images with no face detected: {total_no_face_detected}")
+
+
+if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Face detection and copying tool")
+    parser.add_argument("input_directory", help="Input directory containing images")
+    parser.add_argument("output_directory", help="Output directory of images with detected faces")
+    args = parser.parse_args()
+
+    # Call the function to perform face detection and copying
+    perform_face_detection(args.input_directory, args.output_directory)
